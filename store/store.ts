d1643f4ca07fa5f17b9c693f64ee9510216a3fab
Jkhall81 <jason.kei.hall@gmail.com>
2024-04-21 11:46:20 -0700

    installed zustand to manage state.  couldnt get the event listener to work, so I found another way to get the navbar effect I want on the faq page. next going to work on the home page pictures.

---
+++ store/store.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+
+type NavStore = {
+  navSolid: boolean;
+  setNavSolidTrue: () => void;
+  setNavSolidFalse: () => void;
+};
+
+export const useNavStore = create<NavStore>((set) => ({
+  navSolid: false,
+  setNavSolidTrue: () => {
+    set({ navSolid: true });
+  },
+  setNavSolidFalse: () => {
+    set({ navSolid: false });
+  },
+}));
