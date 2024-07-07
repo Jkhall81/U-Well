067f0e93770712fcd63aba7a890572e4d76faedd
Jkhall81 <jason.kei.hall@gmail.com>
2024-07-06 19:37:42 -0700

    moving env variable console log to server component, use client was coming up undefined.

--- components/account/LogoutButton.tsx
+++ components/account/LogoutButton.tsx
@@ -5,10 +5,6 @@ import { Button } from "@/components/ui/button";
 
 const LogoutButton = () => {
   const handleSignOut = () => {
-    console.log("AUTH_SECRET", process.env.AUTH_SECRET);
-    console.log("DATABASE_URL", process.env.DATABASE_URL);
-    console.log("RESENT_API_KEY", process.env.RESEND_API_KEY);
-    console.log("AUTH_TRUST_HOST", process.env.AUTH_TRUST_HOST);
     signOut();
   };
   return (
