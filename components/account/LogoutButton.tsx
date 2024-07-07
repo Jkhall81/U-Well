dd002c40b5ff562984bf7536d4189082290ce511
Jkhall81 <jason.kei.hall@gmail.com>
2024-07-06 19:28:00 -0700

    still having trouble on amplify, added some console logs to see if the aws amplify environment variables are being accessed correctly.

--- components/account/LogoutButton.tsx
+++ components/account/LogoutButton.tsx
@@ -4,10 +4,17 @@ import { signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 
 const LogoutButton = () => {
+  const handleSignOut = () => {
+    console.log("AUTH_SECRET", process.env.AUTH_SECRET);
+    console.log("DATABASE_URL", process.env.DATABASE_URL);
+    console.log("RESENT_API_KEY", process.env.RESEND_API_KEY);
+    console.log("AUTH_TRUST_HOST", process.env.AUTH_TRUST_HOST);
+    signOut();
+  };
   return (
     <div className="w-full">
       <Button
-        onClick={() => signOut()}
+        onClick={handleSignOut}
         className="h-[60px] w-full rounded-3xl bg-blue-600 text-2xl text-white hover:bg-blue-700 hover:text-white"
       >
         Logout
